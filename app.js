665d7152340171e03cd015e3f92663034a6aa04d
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-04-03 18:49:54 -0700

    Finished Module adding a template engine

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
+
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
 });
 
 // Start Server
@@ -16,3 +31,7 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
+
+
+// function used render HTML templates and send them back to the client
+// res.render('templa te-name', { variables })
